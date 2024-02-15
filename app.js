d5e71c9c376c99281de706fa179d7937f2818c5e
smritipradhan <smritipradhan545@gmail.com>
2024-02-15 08:42:54 +0530

    port

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(8080))
   .catch((err) => console.log(err));
 
 // routes
