d4ed56baf9fe18a1d19e41295c4df0a0e9ce14a0
smritipradhan <smritipradhan545@gmail.com>
2024-02-15 08:26:33 +0530

    cors changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
